c6677a124763a6d0debe862bd7e87f056d54a13e
Heath Stewart <heaths@outlook.com>
2020-02-01 09:20:25 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotnet-depends
+
+A .NET global tool that analyzes and formats assembly dependencies of managed assemblies.
---
+++ src/dotnet-depends/Program.cs
@@ -0,0 +1,18 @@
+// Copyright 2020 Heath Stewart
+// Licensed under the MIT license. See LICENSE.txt in the project root for license information.
+
+using System;
+
+namespace Depends
+{
+    /// <summary>
+    /// Main program.
+    /// </summary>
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
